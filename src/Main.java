65f3bf994dfc09b17c5e55ec40a74fcd5c1a9d80
Sanjay Prabhakar <sanjayprabhakar@sanjays-mbp.attlocal.net>
2018-08-09 22:38:34 -0400

    First commit in isPrimeNumbers to repository

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        int num;
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Write number to check:");
+        num=scanner.nextInt();
+        boolean isPrime=isPrime(num);
+        if(isPrime==true){
+            System.out.println("This number is a prime number");
+        }else{
+            System.out.println("This number is a composite number");
+        }
+}
+
+public static boolean isPrime(int num){
+    for(int i=2;i<num;i++) {
+        if(num%i==0)
+            return false;
+    }
+    return true;
+}
+    }
